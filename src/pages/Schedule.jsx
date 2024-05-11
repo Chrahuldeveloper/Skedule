a5ea6b228a9d40b4bec7d5bc9b1b0b8db4d80873
Rahul <chrahulofficial@gmail.com>
2024-05-11 15:48:00 +0530

    dn

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -4,9 +4,9 @@ import { Calender } from "../components";
 export default function Schedule() {
   return (
     <div className="h-screen bg-[#08090d] ">
-      <div className="pt-7 md:text-center">
-        <h1 className="px-8 text-2xl font-semibold leading-10 pt-7 md:text-3xl text-slate-300 md:max-w-lg md:mx-auto">
-          Schedule your first appotiment 
+      <div className="text-center pt-7">
+        <h1 className="pt-8 text-3xl font-semibold leading-10 md:text-3xl text-slate-300 md:max-w-lg md:mx-auto">
+          Schedule your first appotiment
         </h1>
       </div>
       <Calender />

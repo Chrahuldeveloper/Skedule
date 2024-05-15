69b13c30469b175ac6bb752c484b657d8aa75ca2
Rahul <chrahulofficial@gmail.com>
2024-05-15 10:41:07 +0530

    dn

--- src/pages/Appoitments.jsx
+++ src/pages/Appoitments.jsx
@@ -6,17 +6,28 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 export default function Appoitments() {
   const [userAppointements, setuserAppointements] = useState([]);
   const jwt = sessionStorage.getItem("jwt");
+  const [user, setuser] = useState({
+    Pic: "",
+    Name: "",
+    Bio: "",
+  });
   const provider = new GoogleAuthProvider();
 
   const getuserAppointements = useCallback(async () => {
     try {
       const docref = doc(db, "USERS", jwt);
       const UserData = await getDoc(docref);
-      setuserAppointements(UserData.data().Appointments || []);
+      setuserAppointements(UserData.data()?.Appointments || []);
+      setuser({
+        ...user,
+        Pic: UserData.data()?.photo,
+        Name: UserData.data()?.Name,
+        Bio: UserData.data()?.Bio,
+      });
     } catch (error) {
       console.log(error);
     }
-  }, [jwt]);
+  }, [jwt,user]);
 
   useEffect(() => {
     getuserAppointements();
@@ -43,21 +54,20 @@ export default function Appoitments() {
     }
   };
 
+  console.log(user);
+
   return (
     <div className="w-screen h-screen bg-[#08090d] overflow-y-scroll">
       <div className="px-3 py-8">
         <div className="p-6  border-[1px] border-zinc-800 bg-zinc-900  mx-auto rounded-md max-w-xl">
           <div className="flex flex-col items-center space-y-1.5 text-slate-300">
             <img
-              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=600"
+              src={user?.Pic}
               className="object-cover duration-300 ease-in-out rounded-full cursor-pointer h-32 w-32 hover:brightness-75 border-[1px] border-zinc-800"
               alt=""
             />
-            <h1 className="text-lg font-bold">Rahul</h1>
-            <p className="max-w-xs text-xs text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-              fugit dolores deserunt animi. Odit, sapiente!
-            </p>
+            <h1 className="text-lg font-bold">{user?.Name}</h1>
+            <p className="max-w-xs text-xs text-center">{user?.Bio}</p>
           </div>
           {userAppointements.map((i, idx) => {
             return (

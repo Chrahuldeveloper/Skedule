f138f36ca7367de939497308eb1095f06ab2628b
Rahul <chrahulofficial@gmail.com>
2024-05-20 13:50:10 +0530

    done

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -74,7 +74,7 @@ export default function Signup() {
   return (
     <div className="w-screen h-screen bg-[#08090d]">
       <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 ">
-        <div className="z-50 bg-zinc-900 rounded-md w-[80vw] sm:w-[60vw] md:w-[50vw] lg:w-[40vw] xl:w-[30vw] py-10 flex flex-col justify-center items-center gap-6 px-10 ">
+        <div className="z-50 bg-zinc-900 rounded-md w-[95vw] sm:w-[60vw] md:w-[50vw] lg:w-[40vw] xl:w-[30vw] py-10 flex flex-col justify-center items-center gap-6 px-10 ">
           <div className="space-y-3 lg:text-center">
             <h1 className="text-2xl font-semibold text-slate-300">Sign up </h1>
           </div>
@@ -100,11 +100,9 @@ export default function Signup() {
             onClick={emailPassSignIn}
             className="  font-semibold w-full justify-center gap-3.5 py-3.5 bg-violet-600 text-sm rounded-full flex items-center text-slate-300"
           >
-            <h1 className="text-xs font-semibold">Sign in</h1>
+            <h1 className="text-sm font-semibold">Sign in</h1>
           </button>
-          <div className="w-full mt-2 text-center text-slate-300">
-            ------------------------ OR -------------------------
-          </div>
+          <div className="w-full mt-2 text-center text-slate-300">OR</div>
 
           <button
             onClick={GoogleSignIn}

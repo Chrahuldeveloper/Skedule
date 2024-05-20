761ebbc984263f9c974ee178a1e7c22344ec4d7a
Rahul <chrahulofficial@gmail.com>
2024-05-20 15:43:28 +0530

    dn

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -75,7 +75,7 @@ export default function Signup() {
   return (
     <div className="w-screen h-screen bg-[#08090d]">
       <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 ">
-        <div className="z-50 bg-zinc-900 rounded-md w-[95vw] sm:w-[60vw] md:w-[50vw] lg:w-[40vw] xl:w-[30vw] py-10 flex flex-col justify-center items-center gap-6 px-10 ">
+        <div className="z-50 bg-zinc-900 rounded-md w-[95vw] sm:w-[60vw] md:w-[50vw] lg:w-[40vw]  py-10 flex flex-col justify-center items-center gap-6 px-10 ">
           <input
             type="text"
             placeholder="Email"
@@ -83,7 +83,7 @@ export default function Signup() {
             onChange={(e) => {
               setuser({ ...user, email: e.target.value });
             }}
-            className="border-[1px] rounded-full border-zinc-800 px-5 py-3 outline-none bg-transparent text-slate-300 w-[80vw]"
+            className="border-[1px] rounded-full border-zinc-800 px-5 py-3 outline-none bg-transparent text-slate-300 w-[80vw] sm:w-[60vw] md:w-[50vw] lg:w-[30vw]"
           />
           <input
             type="password"
@@ -92,11 +92,11 @@ export default function Signup() {
             onChange={(e) => {
               setuser({ ...user, Pass: e.target.value });
             }}
-            className="border-[1px] rounded-full border-zinc-800 px-5 py-3 outline-none bg-transparent text-slate-300 w-[80vw]"
+            className="border-[1px] rounded-full border-zinc-800 px-5 py-3 outline-none bg-transparent text-slate-300 w-[80vw] sm:w-[60vw] md:w-[50vw] lg:w-[30vw]"
           />
           <button
             onClick={emailPassSignIn}
-            className="  w-[80vw] font-semibold  justify-center gap-3.5 py-3.5 bg-violet-600 text-sm rounded-full flex items-center text-slate-300"
+            className=" sm:w-[60vw] md:w-[50vw] lg:w-[30vw] w-[80vw] font-semibold  justify-center gap-3.5 py-3.5 bg-violet-600 text-sm rounded-full flex items-center text-slate-300"
           >
             <h1 className="text-sm font-semibold">Sign in</h1>
           </button>
@@ -104,7 +104,7 @@ export default function Signup() {
 
           <button
             onClick={GoogleSignIn}
-            className=" w-[80vw]  font-semibold justify-center gap-3.5 py-3.5 bg-violet-600 text-sm rounded-full flex items-center text-slate-300"
+            className=" w-[80vw] sm:w-[60vw] md:w-[50vw] lg:w-[30vw] font-semibold justify-center gap-3.5 py-3.5 bg-violet-600 text-sm rounded-full flex items-center text-slate-300"
           >
             <img src={logo} className="w-7 h-7" alt="" />
             <h1 className="text-xs font-semibold">Sign in with Google</h1>

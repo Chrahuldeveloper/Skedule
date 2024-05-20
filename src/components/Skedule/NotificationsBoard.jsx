e81b437bb26f42e358d1951797ef4f3a9e3cfb9c
Rahul <chrahulofficial@gmail.com>
2024-05-20 14:24:22 +0530

    Done

--- src/components/Skedule/NotificationsBoard.jsx
+++ src/components/Skedule/NotificationsBoard.jsx
@@ -46,7 +46,7 @@ export default function NotificationsBoard({ jwt }) {
       <div className="bg-zinc-900 my-6  py-5 border-[1px] rounded-md  border-zinc-800 lg:ml-96 w-[95vw] sm:w-[60vw] mx-auto lg:mx-0 overflow-y-scroll h-[80vh] z-50 ">
         {isloading ? <Loader /> : null}
         <div>
-          <h1 className="px-4 text-2xl font-semibold text-slate-300">
+          <h1 className="px-4 text-sm font-semibold md:text-2xl text-slate-300">
             Your Notifications
           </h1>
         </div>

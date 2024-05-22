96e9915602c215a1a9557e9e6657b5e54f458b79
Rahul <chrahulofficial@gmail.com>
2024-05-22 13:02:28 +0530

    done

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmUpbp_KlNpzOPFXMSl-VDI5ulu3ZpkM0",
-  authDomain: "skedule-ad781.firebaseapp.com",
-  projectId: "skedule-ad781",
-  storageBucket: "skedule-ad781.appspot.com",
-  messagingSenderId: "672236081486",
-  appId: "1:672236081486:web:12537fb7f5615314108e1f",
-  measurementId: "G-HZPCSVXP90",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);

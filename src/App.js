503b820be747daf093f7f08f6c47eba768cb2215
Rahul <chrahulofficial@gmail.com>
2024-06-24 11:26:11 +0530

    done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ import {
 } from "./pages/index";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 export default function App() {
+  AOS.init();
 
   return (
     <Routes>
